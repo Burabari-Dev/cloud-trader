bcb0691465865e6c5c173fd55e44c7c0f78b7d86
Burabari <burabaridev@gmail.com>
2023-10-22 17:18:51 +0100

    change updateRecord to newOrReplaceRecord

--- functions/db/save-session/saveSession.mjs
+++ functions/db/save-session/saveSession.mjs
@@ -1,5 +1,5 @@
 /*global fetch*/
-import { updateRecord } from "../../opt/dynamoDb.mjs";
+import { newOrReplaceRecord } from "../../opt/dynamoDb.mjs";
 
 const sessionKeyValue = process.env.SESSION_TABLE_KEY_VALUE;
 const SESSION_KEY = { sessionID: sessionKeyValue };
@@ -8,6 +8,6 @@ const tableName = process.env.SESSION_TABLE_NAME;
 export const handler = async (event) => {
   const session = event.session;
   const sessionWithKey = { ...session, ...SESSION_KEY };
-  const response = updateRecord(tableName, SESSION_KEY, sessionWithKey);
+  const response = await newOrReplaceRecord(tableName, sessionWithKey);
   console.log('>>---> ', response);                                 //TODO: DELETE THIS LINE
 }

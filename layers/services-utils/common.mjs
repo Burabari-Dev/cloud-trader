6c441d151778dc60777a47d1981eac7f89739a6e
Burabari <burabaridev@gmail.com>
2023-10-24 04:30:24 +0100

    explain why 9.8 seconds instead of 10 in comments

--- layers/services-utils/common.mjs
+++ layers/services-utils/common.mjs
@@ -11,5 +11,5 @@ export const nowTime = () => (moment().format());
 
 export const is10MinElapsed = (time = '2013-02-08 24:00:00.000') => {
   const diff = moment().diff(moment(time), 'minutes', true);
-  return diff > Math.abs(9.8);
+  return diff > Math.abs(9.8);  //-> Capital.com session expires in 10 mins. Use 9.8 to account for aws services processing time
 }
